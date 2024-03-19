0af700a717049764b5e89e565ddcb13bd0239fc3
Cameron Posey <cameronposey@outlook.com>
2024-03-19 01:45:15 -0700

    Simplify path conditional logic

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const EventEmitter = require('node:events');
 const fs = require('fs');
 const http = require('node:http');
+const url = require('node:url');
 const eventEmitter = new EventEmitter();
 
 eventEmitter.on('start', (num) => {
@@ -8,13 +9,12 @@ eventEmitter.on('start', (num) => {
 });
 
 http.createServer((req, res) => {
+    let q = url.parse(req.url);
     let path = '';
-    if (req.url === '/'){
+    if (q.path === '/'){
         path = 'index.html';
-    } else if (req.url === '/contact-me'){
-        path = 'contact-me.html'
-    } else if (req.url === '/about'){
-        path = 'about.html';
+    } else if (q.path === '/contact-me' || q.path === '/about' ){
+        path = `${q.path.substring(1)}.html`;
     } else {
         path = '404.html';
     }
