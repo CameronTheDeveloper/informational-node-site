73e9ddf10a250fff0734aaed3fa4ee1b813efde8
CameronTheDeveloper <Cameronposey@outlook.com>
2024-04-20 01:31:30 -0700

    Add about.html and contact-me.html routes

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ app.get('/', (req, res) => {
     res.sendFile("index.html", { root: __dirname });
 });
 
+app.get('/about', (req, res) => {
+    res.sendFile("about.html", { root: __dirname });
+});
+
+app.get('/contact-me', (req, res) => {
+    res.sendFile("contact-me.html", { root: __dirname });
+});
+
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
 });
